e6a844fa06eb7b977d370c33540dd58fa212838d
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-11 19:27:31 +0100

    add ex 4

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -1,7 +1,26 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+// - creo una funzione che accetta un parametro
+// - itero con un for la mia parola (lettera per lettera)
+// - controllo con un if se la lettera corrente è == a o == e o == i
 
 const word = 'javascript';
+const vocali = (word) => {
+    let contatore = 0;
+    let listaVocali = [];
+    for (let i = 0; i < word.length; i++) {
+        let elements = word[i];
+        
+        if (elements === 'a'|| elements === 'e'|| elements === 'i'|| elements === 'o'|| elements === 'u') {
+        
+        listaVocali.push(elements)    
+        contatore++
 
+     }   
+    }
+    return [contatore,listaVocali];
+    
+}
+console.log(vocali(word));
 
 // Dichiara la funzione qui.
 
