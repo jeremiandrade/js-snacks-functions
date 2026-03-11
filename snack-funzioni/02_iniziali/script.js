4606c9cb76b5fa0c88042866f8e7198837dd77fd
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-11 06:23:12 +0100

    add ex n2

--- snack-funzioni/02_iniziali/script.js
+++ snack-funzioni/02_iniziali/script.js
@@ -1,6 +1,9 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+const elements = (name) =>name.map(initial => initial[0]) 
+console.log(elements(names));
+
 
 
 // Dichiara la funzione qui.
