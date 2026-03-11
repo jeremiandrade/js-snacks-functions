fa16f4542667d346a912af19acc63eb06eb5ba8e
jeremiandrade <jeremiandrade04@gmail.com>
2026-03-11 05:17:51 +0100

    first commit

---
+++ snack-funzioni/01_saluti/script.js
@@ -0,0 +1,18 @@
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+
+const userName = 'Mario';
+
+const hello = (userName) => {
+    const togreet = 'ciao';
+    const somma = togreet + userName;
+    return somma;
+}
+console.log(hello(' ' + 'mario'));
+
+const greet = (userName) => 'ciao'+ userName
+console.log(hello(' ' + 'Mario'))
+
+
+// Dichiara la funzione qui.
+// Invoca la funzione qui e stampa il risultato in console
+//Risultato atteso se si passa 'Mario': // ciao Mario
